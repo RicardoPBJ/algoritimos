7c662899a9e4be10e28970705b07f94c7dffe09c
Ricardo <rbrumjunio@gmail.com>
2023-06-16 15:35:01 -0300

    Número de estudantes estudando no mesmo horário (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    try:
+        count = 0
+        for p in permanence_period:
+            if p[0] <= target_time <= p[1]:
+                count += 1
+        return count
+    except Exception:
+        return None
