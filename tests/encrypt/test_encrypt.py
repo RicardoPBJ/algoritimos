30a126e290fb7e6fb2db9c7d8b9efd95693ef3d3
Ricardo <rbrumjunio@gmail.com>
2023-06-19 13:44:43 -0300

    Criptografia de inversões (Testes)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(100, 10)
+    
+    with pytest.raises(TypeError):
+        encrypt_message('', '')
+
+    assert encrypt_message('encrypt', 2) == 'tpyrc_ne'
+    assert encrypt_message('encrypt', 3) == 'cne_tpyr'
+    assert encrypt_message('encrypt', 20) == 'tpyrcne'
