4e36800e70728a642a370eb893cd23478da3faef
Ricardo <rbrumjunio@gmail.com>
2023-06-19 16:08:43 -0300

    Palíndromos (Iteratividade)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    low_index = 0
+    high_index = len(word)
+
+    if len(word) == 0:
+        return False
+
+    while low_index < high_index:
+        if word[low_index] != word[high_index - 1]:
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -5,7 +5,6 @@ from challenges.challenge_encrypt_message import encrypt_message
 def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message(100, 10)
-    
     with pytest.raises(TypeError):
         encrypt_message('', '')
 
