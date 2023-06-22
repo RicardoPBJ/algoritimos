3a69b227a8974b8f7769a64573b142047895dd4a
Ricardo <rbrumjunio@gmail.com>
2023-06-21 23:42:34 -0300

    Encontrando números repetidos (Algoritmo de busca)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,28 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+from typing import List
+
+
+def finder(nums, count):
+
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        elif num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
+
+
+def find_duplicate(nums: List[int]):
+
+    if len(nums) <= 1 or not nums:
+        return False
+
+    count = {}
+
+    finder(nums, count)
+
+    for num, counter in count.items():
+        if counter > 1:
+            return num
+
+    return False
